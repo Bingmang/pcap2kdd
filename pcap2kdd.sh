00fea3811b28ee6e6b00f6870d46fd2f8b3fe78c
bingmang <ustbbingmang@gmail.com>
2018-10-23 18:25:33 +0800

    [update] pcap2kdd.sh

--- pcap2kdd.sh
+++ pcap2kdd.sh
@@ -2,5 +2,6 @@
 bro -r $1 darpa2gurekddcup.bro > conn.list
 sort -n conn.list > conn_sort.list
 ./trafAld.out conn_sort.list
-python kdd.py trafAld.list
-mv trafAld.list "feats/${1##*/}.feat"
+python3 kdd.py trafAld.list
+mv kqout "feats/${1##*/}.txt"
+echo "Done - ${1##*/}"
