14dbf131bf8fcf3580430f9350a51c55e08bd6ea
Bingmang <ustbbingmang@foxmail.com>
2018-10-23 17:08:20 +0800

    [update] 更新过滤策略: 删除所有重复连接

--- README.md
+++ README.md
@@ -20,52 +20,39 @@ Then it will generate your_data.feat
 ## 3. Features
 
 For each connection the attributes of .feat file: 
-- num_conn
-- startTimet
-- orig_pt
-- resp_pt
-- orig_ht
-- resp_ht
-- duration
-- protocol
-- resp_pt
-- flag
-- src_bytes
-- dst_bytes
-- land
-- wrong_fragment
-- urg
-- hot
-- num_failed_logins
-- logged_in
-- num_compromised
-- root_shell
-- su_attempted
-- num_root
-- num_file_creations
-- num_shells
-- num_access_files
-- num_outbound_cmds
-- is_hot_login
-- is_guest_login
-- count_sec
-- srv_count_sec
-- serror_rate_sec
-- srv_serror_rate_sec
-- rerror_rate_sec
-- srv_error_rate_sec
-- same_srv_rate_sec
-- diff_srv_rate_sec
-- srv_diff_host_rate_sec
-- count_100
-- srv_count_100
-- same_srv_rate_100
-- diff_srv_rate_100
-- same_src_port_rate_100
-- srv_diff_host_rate_100
-- serror_rate_100
-- srv_serror_rate_100
-- rerror_rate_100
-- srv_rerror_rate_100
+- 0  num_conn
+- 1  startTimet
+- 2  orig_pt
+- 3  resp_pt
+- 4  orig_ht
+- 5  resp_ht
+- 6  duration
+- 7  protocol
+- 8  resp_pt
+- 9  flag
+- 10 src_bytes
+- 11 dst_bytes
+- 12 land
+- 13 wrong_fragment
+- 14 urg
+- 15 count_sec
+- 16 srv_count_sec
+- 17 serror_rate_sec
+- 18 srv_serror_rate_sec
+- 19 rerror_rate_sec
+- 20 srv_error_rate_sec
+- 21 same_srv_rate_sec
+- 22 diff_srv_rate_sec
+- 23 srv_diff_host_rate_sec
+- 24 count_100
+- 25 srv_count_100
+- 26 same_srv_rate_100
+- 27 diff_srv_rate_100
+- 28 same_src_port_rate_100
+- 29 srv_diff_host_rate_100
+- 30 serror_rate_100
+- 31 srv_serror_rate_100
+- 32 rerror_rate_100
+- 33 srv_rerror_rate_100
 
-total 47 features.
+total 34 features.
--- kdd.py
+++ kdd.py
@@ -1,4 +1,5 @@
 import sys
+from collections import OrderedDict
 
 if len(sys.argv) > 1:
     fname = sys.argv[1]
@@ -6,31 +7,51 @@ if len(sys.argv) > 1:
 SPLIT_SYMBOL = ' '
 
 # remove duplicated row according to ip and port
-def removeDuplicated(filepath):
-    conn_num = 0
+def scan_file(filepath):
     cache_dict = {}
     cache_array = []
     fd = open(filepath, 'r')
     for line in fd.readlines():
         features = line.split()
-        if features[7] == 'icmp':
-            continue
-        features[0] = str(conn_num) # TODO convert to int
+        # 15~27 是 kdd 的 10-22 特征 无法提取 都是0
+        del features[15:28]
         address_port = features[2:6]
         cache_key = ' '.join(address_port)
+
         if cache_dict.get(cache_key):
             cache_dict[cache_key] += 1
+            continue
         else:
             cache_dict[cache_key] = 1
             cache_array.append(features)
-            conn_num += 1
+
+    def cache_filter(x):
+        address_port = x[2:6]
+        cache_key = ' '.join(address_port)
+        return cache_dict[cache_key] == 1
+
+    cache_array = list(filter(cache_filter, cache_array))
+    
+    conn_num = 0
+    for index in range(len(cache_array)):
+        conn_num += 1
+        cache_array[index][0] = str(conn_num)
+
+    duplicate_count = 0
+    duplicate_number = 0
+    for key in cache_dict:
+        if cache_dict[key] > 1:
+            duplicate_count += 1
+            duplicate_number += cache_dict[key]
+            # print(key, '重复次数:', cache_dict[key])
+    print('总共重复次数:', duplicate_count, duplicate_number)
     fo = open('kqout', 'w')
     fo.writelines(map(lambda features: SPLIT_SYMBOL.join(features) + '\n', cache_array))
     fo.close()
     fd.close()
 
 def main():
-    removeDuplicated(fname)
+    scan_file(fname)
 
 if __name__ == '__main__':
     main()
